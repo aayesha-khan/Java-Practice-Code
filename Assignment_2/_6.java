218cdd47acf6ed72b8d7ee4c1fc482d72812a4ae
Aayesha Khan <aayeshabkhan2310@gmail.com>
2026-04-10 19:03:22 +0530

    _6.java

---
+++ Assignment_2/_6.java
@@ -0,0 +1,10 @@
+
+// 6. Create a class WordCounter.
+//  Accept a sentence from the user.
+//  Use StringTokenizer to count words.
+
+//  Display total number of words.
+
+public class _6Sixth {
+    
+}
